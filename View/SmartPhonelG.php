212a1f876f638cb5eb4fae21a97ed76a309f9de0
jcaguado <julio.cesar.aguado@hotmail.com>
2017-11-20 23:46:23 -0500

    Update SmartPhonelG.php

--- View/SmartPhonelG.php
+++ View/SmartPhonelG.php
@@ -93,8 +93,54 @@
                     <td  > 
      
                         <font size="3">
-                            1.xcffxgfgcfghfghcfghggh <br>
-                            2. sdfjbdsgfdsjf
+                          Como formatear un celular LG <br>
+
+
+Si estas buscando como formatear un celular LG , puede ser por los siguientes problemas con tu Smartphone LG: <br>
+
+
+ <br>
+*No logras sincronizar aplicaciones o te van muy lentas <br>
+*Problemas con el sistema operativo <br>
+*Que quieras vender – regalar tu celular, y lo quieres entregar limpio <br>
+*Tu celular va super lento <br>
+O quien sabe que otra cosa pase para que estés buscando en Internet: Como formatear un celular LG <br>
+ <br>
+<br>
+No es un problema, ya que en esta ocasión vamos a enseñarte Como formatear un celular LG con 2 métodos seguros <br>
+y fáciles de realizar, solo sigue los pasos que te daremos a continuación <br>
+<br>
+Ahora haremos el formateo con un celular LG Optimus L90: lo primero que haremos es hacer un backup sobre mis archivos <br>
+almacenados en la memoria interna o lo exportaremos a la memoria externa SD, y hare un backup de mis contactos que lo <br>
+puedes realizar desde las mismas opciones del móvil. <br>
+Procedemos a explicarte como formatear un celular LG  :  <br>
+
+<br>
+<br>
+<br>
+ 
+Método 1:<br>
+<br>
+1. Iremos “Ajustes” – General – Copia de respaldo y restauración, seleccionamos datos de fábrica, restaurar teléfono y le damos en borrar todos. <br>
+2. Luego iniciara tu móvil en modo avión, nuevamente lo reinicias y vuelves a encender y lo veras como en modo de la primera vez que lo adquiriste.<br>
+3. Hard Reset, este es un formateo general sobre todo si tu móvil se encuentra con alguna contraseña que evite el formateo, entonces para ello lo haremos <br>
+con los siguientes pasos.<br>
+4. Procedemos a quitar la batería del móvil y presionamos el botón de volumen (-) y el botón de encendido juntos, una vez aparecido el logo de LG encendido,<br>
+ sueltas el botón de encendido y rápidamente lo vuelve a presionar.<br>
+5. Va aparecer un menú y le das en “YES” y le das con el botón de encendido, nuevamente te pedirá confirmación y seleccionas lo mismo, el móvil comenzó a<br>
+ reiniciar y por primera vez demorar en iniciar, esperamos unos 40 segundos y tendremos que volver a configurar nuestra cuenta de LG y Google. <br>
+<br>
+Si aun no solucionaste tu duda sobre Como formatear un celular LG  y En caso el móvil insista que no se puede formatear lo tendremos que hacer con ayuda <br>
+de una PC y para ello seguiremos los siguientes pasos:<br>
+<br>
+Método 2:<br>
+<br>
+1. Primero conectamos la tablet a una PC y nos vamos a “Ajustes” – Opciones de desarrollo, y luego activamos la palomita, depuración USB. <br>
+2. Luego nos dirigimos a la PC y buscamos el programa “ejecutar” y escribimos “cmd” y le damos aceptar, aparecerá la consola de comando y escribimos <br>
+“Adb shell” damos enter, y escribimos ” wipe data” damos enter y nuevamente escribimos “reboot sistema”. <br>
+3. Como verán el celular comenzara a reiniciarse y ya se han eliminado todos los datos, y nos queda configurarla con agregando nuestra cuenta de Gmail, <br>
+etc. La primera vez demorar en encender la tablet así que esta parte hay que tener paciencia. <br>
+                            
                         </font>
 
                     </td>
