646e0acc605c9daa5bbc41a7c459249670fb1106
jcaguado <julio.cesar.aguado@hotmail.com>
2017-11-25 13:24:04 -0500

    Add files via upload
    version 1 con video

---
+++ TVLG.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous">
+
+
+
+    <link rel="stylesheet" type="text/css" href="Style.css">
+
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+
+    <meta charset="utf-8" />
+    <title>Practica bootstrap</title>
+</head>
+
+<body>
+    <div class="container" style="background-color: #4285F4;">
+
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <img src="../Imagenes/tutotics.png" alt="">
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <center>
+                <br>
+
+
+
+                <font ui-ref="mail" style="
+                text-shadow: 0 1px 0 #ccc, 
+                0 2px 0 #c9c9c9,
+                0 3px 0 #bbb,
+                0 4px 0 #b9b9b9,
+                0 5px 0 #aaa,
+                0 6px 1px rgba(0,0,0,.1),
+                0 0 5px rgba(0,0,0,.1),
+                0 1px 3px rgba(0,0,0,.3),
+                0 3px 5px rgba(0,0,0,.2),
+                0 5px 10px rgba(0,0,0,.25),
+                0 10px 10px rgba(0,0,0,.2),
+                0 20px 20px rgba(0,0,0,.15);
+                color: #fff;
+                font-size: 70px;
+                font-family: Impact; 
+                opacity: 0.8;  ">SMART TV LG</font>
+            </center>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-3">
+
+        </div>
+    </div>
+
+    <div class="container">
+        <center>
+            <table>
+                <tr>
+                    <td>
+                        <form action="TV.html">
+                            <input type="image" class="punteros" title="Volver" src="../Imagenes/PunteroIzq.png">
+                        </form>
+                    </td>
+                    <td>
+                        <form action="Tutotic.html">
+                            <input type="image" class="punteros" title="Volver" src="../Imagenes/home.png">
+                        </form>
+                    </td>
+                </tr>
+            </table>
+        </center>
+    </div>
+
+    <div class="container">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <br>
+            <table>
+                <tr>
+                    <td class="productos">
+                        <label for="1">
+                            <h3> configuracion basica de un SmartTv LG </h3>
+                        </label>
+                        <form>
+                            <input type="image" class="img2" id="3" src="../Imagenes/LG_G.png">
+                        </form>
+                    </td>
+                    <td>
+
+                        <font size="3">
+                          Para configurar la conexión en un televisor de LG <br>
+deberás pulsar el botón Configuración de tu mando. <br>
+Si no tienes este botón, entonces pulsa Home y luego <br>
+ con las flechas seleccionar la opción Configuración. <br>
+<br>
+Conexión a internet de LG Smart TV <br>
+<br>
+
+Te aparecerá un menú lateral a la izquierda de la <br>
+pantalla. Desplázate hacia abajo hasta un icono <br>
+con una bola del mundo. Dentro de él, elige Conexiones <br> 
+de Red y después Iniciar conexión. <br>
+<br>
+Si has conectado la tele mediante cable de red, selecciona <br>
+la primera opción. Si no, te aparecerá una lista de redes <br>
+WiFi entre las que tendrás que elegir la tuya e introducir <br>
+su contraseña. <br>
+<br>
+
+Si quieres descargar aplicaciones, también tendrás que <br>
+crear una cuenta en los servicios de LG. Podrás e hacerlo <br>
+ cuando elijas una. <br>
+                        </font>
+
+                    </td>
+                </tr>
+
+            </table>
+
+            <iframe width="854" height="480" src="https://www.youtube.com/embed/GyOocViIqeY" frameborder="0" gesture="media" allowfullscreen></iframe>
+        </div>
+    </div>
+
+</body>
+
+</html>
